4797294adebfd9b51c3eaaa3624b280fc83ca14c
Leanne <lemeyer.no@gmail.com>
2024-05-12 22:09:11 +0200

    updated

--- JS/GlobalConst.mjs
+++ JS/GlobalConst.mjs
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://v2.api.noroff.dev";
+export const API_BASE_URL = "https://v2.api.noroff.dev/auth/register";
 
 
 export const API_REG_URL = API_BASE_URL;
--- JS/register.mjs
+++ JS/register.mjs
@@ -1,24 +1,52 @@
-import { regUser } from "./API/regUser.mjs";
+import { API_REG_URL } from "./GlobalConst.mjs";
 
 /* Constants */
 const regForm = document.getElementById('reg-form');
 
-
-
-
-
 /* EVENT LISTENERS */
-regForm.addEventListener('submit', registerUser);
+
+regForm.addEventListener('submit', formSubmit);
 
 /* FUNCTIONS */
-function registerUser(e) {
-  e.preventDefault();
 
-  const regForm = e.target;
-  const regFormInputs = new FormData(regForm);
-  const userProfile = Object.fromEntries(regFormInputs.entries());
-
-  regUser(userProfile);
+async function formSubmit(event) {
+  event.preventDefault();
+  const regFormData = new FormData(regForm);
+  const formDataObject = Object.fromEntries(regFormData);
+  const API_URL = API_REG_URL;
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formDataObject),
+  });
+  const data = await response.json();
+  console.log('User registered', data);
 }
+    try {
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formDataObject),
+      });
+
+      const data = await response.json();
+      console.log('User registered', data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+
+
+
+
+/* FUNCTION CALLS */
+
+
+
+
+
 
 
