6ad4b8a6f3a54fc4b808812c4cb9792a0ddade9b
Leanne <lemeyer.no@gmail.com>
2024-05-14 10:05:10 +0200

    updated.

--- JS/edit.js
+++ JS/edit.js
@@ -11,14 +11,28 @@ function logout() {
 
   const accessToken = localStorage.getItem('accessToken');
 
-  localStorage.removeItem('accessToken');
 
-  window.location.href = '/account/login.html';
-
+ 
   if(accessToken === null) {
-    alert('You have successfully logged out!');
+    alert('You are already logged out!');
+    return;
+    
+  } else if(accessToken !== null) {
+    localStorage.removeItem('accessToken');
+    window.location.href = '/account/login.html';
+   alert('You have successfully logged out!');
+
+   
   }
+
   else {
     alert('An error has occurred. Please try again.');
   }
+
+  console.log('User logged out', accessToken);
+
+
+
 }
+
+ 
