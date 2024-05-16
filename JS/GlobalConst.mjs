705814bbf5cf1ac4ba0c131d4efea49f78522ec8
Leanne <lemeyer.no@gmail.com>
2024-05-16 10:18:01 +0200

    updated

--- JS/GlobalConst.mjs
+++ JS/GlobalConst.mjs
@@ -9,4 +9,3 @@ export const API_LOGIN_URL = LOGIN_API_URL;
 
 export const CREATE_POST_API_URL = "https://v2.api.noroff.dev/blog/posts/Leanne002";
 
-
--- JS/edit.mjs
+++ JS/edit.mjs
@@ -1,3 +1,4 @@
+import { CREATE_POST_API_URL } from "./GlobalConst.mjs";
 
 const logoutLink= document.getElementById('logout-link-edit');
 
@@ -36,10 +37,53 @@ export function logout() {
   console.log('User logged out', accessToken);
 
 }
+//Function for Fetching Blog posts
+async function fetchBlogPosts() {
+  const API_URL = CREATE_POST_API_URL;
+  const response = await fetch(API_URL, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
+  const blogPosts = await response.json();
+  console.log(blogPosts);
 
+  displayBlogPosts();
+}
 
- 
+//Function for displaying the blog posts
+function displayBlogPosts() {
+  const blogPostsWrapper = document.getElementsByClassName('blog-posts-wrapper');
+
+  blogPosts.forEach(post => {
+    const blogPostBox = document.createElement("div");
+    const image = document.createElement("img");
+    const headerContainer = document.createElement("div");
+    const header = document.createElement("h2");
+    const buttonWrapper = document.createElement("div");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    blogPostBox.classList.add('blog-post');
+    headerContainer.classList.add('header-container');
+    header.classList.add('h2-Edit-Page');
+    buttonWrapper.classList.add('btn-wrapper');
+    editButton.classList.add('edit-page-btn');
+    deleteButton.classList.add('edit-page-btn');
+
+    image.src = post.image;
+    header.textContent = post.title;
+    editButton.textContent = 'Edit';
+    deleteButton.textContent = 'Delete';
+
+    blogPostBox.appendChild(image, headerContainer, buttonWrapper);
+    headerContainer.appendChild(header);
+    buttonWrapper.appendChild(editButton, deleteButton);
+    blogPostsWrapper.appendChild(blogPostBox);
+  });
+}
 
 //FUNCTION CALLS
-
+fetchBlogPosts();
