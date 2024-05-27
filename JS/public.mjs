5c722f258d96dc4143938a20eb7440aa3333a04b
Leanne <lemeyer.no@gmail.com>
2024-05-27 21:24:29 +0200

    updated
    updated

--- JS/public.mjs
+++ JS/public.mjs
@@ -6,22 +6,15 @@
 
 
 //FUNCTIONS
-
-//FUNCTION FOR FETCHING CLICKED POST
-
-
 async function fetchClickedPost() {
   const clickedPost = JSON.parse(localStorage.getItem('clickedBlogPost'));
   createPostHtml(clickedPost);
   const postId = clickedPost.id;
 
-
   const url = new URL(window.location.href);
   history.pushState({}, "", `${postId}`);
   url.searchParams.set('postId', `${postId}`); 
   //Looked to this website for help inserting the post ID into the URL : https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
-
-
 }
 
 //FUNCTION TO CREATE HTML FOR CLICKED POST
@@ -35,10 +28,14 @@ function createPostHtml(clickedPost) {
   body.textContent = clickedPost.body;
 
   const author = document.querySelector('.Author');
-
   author.innerHTML = "Author:" + clickedPost.author.name;
   const publishedDate = document.querySelector('.Published-date');
   publishedDate.innerHTML = "Published:" + clickedPost.created;
 }
+
+
+
+
+
 
 fetchClickedPost();
