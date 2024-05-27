8bd3ace983cbaba0a03267bdf346b232f0192c01
Leanne <lemeyer.no@gmail.com>
2024-05-27 21:38:36 +0200

    updated
    updated

--- JS/home.mjs
+++ JS/home.mjs
@@ -199,9 +199,8 @@ let last12Posts = posts ? posts.slice(-12) : [];
 function displayCarouselPosts() {
 
 
-
-  let posts = JSON.parse(localStorage.getItem('blogPosts'));
-  let last12Posts = posts ? posts.slice(-12) : [];
+let posts = JSON.parse(localStorage.getItem('blogPosts'));
+let last12Posts = posts ? posts.slice(-12) : [];
 
 const carouselImage1= document.getElementById('carousel-image-1');
 
