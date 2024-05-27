cd547bc9bdb5cc7ff25758ba08be732d9b96ec35
Leanne <lemeyer.no@gmail.com>
2024-05-27 20:13:01 +0200

    upd
    upd

--- JS/home.mjs
+++ JS/home.mjs
@@ -176,9 +176,13 @@ async function fetchBlogPosts() {
     const posts = await response.json();
     const blogPosts = posts.data;
     localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+    displayCarouselPosts();
   } catch (error) {
     console.error('Error:', error);
   }
+
+
+
 }
 
 
@@ -332,7 +336,7 @@ selectElement.addEventListener('change', (event) => {
 
   //**CALL FUNCTIONS */
 
-  displayCarouselPosts();
+
   displayGridPosts();
   fetchBlogPosts();
   postUserData();
