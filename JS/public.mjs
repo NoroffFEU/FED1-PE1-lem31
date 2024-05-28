0c3fa16ee40fea3d61fb712b458caa2cf36457a2
Leanne <lemeyer.no@gmail.com>
2024-05-28 09:53:09 +0200

    updated
    updated

--- JS/public.mjs
+++ JS/public.mjs
@@ -13,24 +13,35 @@ async function fetchClickedPost() {
   const url = new URL(window.location.href);
   url.searchParams.set('postId', `${postId}`);
   history.pushState({}, "", url.href);
-  createPostHtml(clickedPost);
+  await createPostHtml(clickedPost);
 }
 
 //FUNCTION TO CREATE HTML FOR CLICKED POST
-function createPostHtml(clickedPost) {
-  
+async function createPostHtml(clickedPost) {
   const title = document.querySelector('.h1-public-blog-post');
-  title.innerHTML = clickedPost.title;
+  if (title) {
+    title.innerHTML = clickedPost.title;
+  }
  
   const image = document.querySelector('.Public-Blog-Post-Img');
-  image.src = clickedPost.media ? clickedPost.media.url : '';
+  if (image) {
+    image.src = clickedPost.media ? clickedPost.media.url : '';
+  }
+  
   const body = document.querySelector('.public-post-body');
-  body.textContent = clickedPost.body;
+  if (body) {
+    body.textContent = clickedPost.body;
+  }
 
   const author = document.querySelector('.Author');
-  author.innerHTML = "Author:" + clickedPost.author.name;
+  if (author) {
+    author.innerHTML = "Author:" + clickedPost.author.name;
+  }
+  
   const publishedDate = document.querySelector('.Published-date');
-  publishedDate.innerHTML = "Published:" + clickedPost.created;
+  if (publishedDate) {
+    publishedDate.innerHTML = "Published:" + clickedPost.created;
+  }
 }
 
 
