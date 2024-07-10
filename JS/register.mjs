ce4b7f4b27ab34c13db4ed4f4326095c8f2f2c17
Leanne <lemeyer.no@gmail.com>
2024-07-10 10:01:42 +0200

    updated
    updated error handling register page

--- JS/register.mjs
+++ JS/register.mjs
@@ -1,11 +1,11 @@
 import { API_REG_URL } from "./GlobalConst.mjs";
 
 /* Constants */
-const regForm = document.getElementById('reg-form');
+const regForm = document.getElementById("reg-form");
 
 /* EVENT LISTENERS */
 
-regForm.addEventListener('submit', regFormSubmit);
+regForm.addEventListener("submit", regFormSubmit);
 
 /* FUNCTIONS */
 
@@ -16,37 +16,22 @@ async function regFormSubmit(event) {
   const API_URL = API_REG_URL;
   try {
     const response = await fetch(API_URL, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(formDataObject),
     });
 
-    // const data = await response.json();
-  
-
     if (response.status === 201) {
-      alert('You have successfully registered an account!');
-      window.location.href = 'login.html';
+      alert("You have successfully registered an account!");
+      window.location.href = "login.html";
+    } else if (response.status === 400) {
+      alert("This user already exists, please try again.");
     } else {
-      alert('Something went wrong, please try again.');
+      alert("Something went wrong, please try again.");
     }
- 
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
